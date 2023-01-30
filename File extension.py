546d50605d1865cdf48262d381da41ee93a8d8fe
Deepakchandhru <122144025+Deepakchandhru@users.noreply.github.com>
2023-01-30 17:38:24 +0530

    Update File extension.py

--- File extension.py
+++ File extension.py
@@ -1,7 +1,7 @@
 file=list(input("Enter file name:").split("."))
 if file[1]=="py":
      print("Python")
-elif file[1]=="xlsx"
+elif file[1]=="xlsx":
      print("Excel")
 elif file[1]=="ppt"
      print("PowerPoint")
