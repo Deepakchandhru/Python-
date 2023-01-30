bffc826b876f9e389d502171033fe986bd3cf96c
Deepakchandhru <122144025+Deepakchandhru@users.noreply.github.com>
2023-01-30 20:56:21 +0530

    Update uni_admin.py

--- uni_admin.py
+++ uni_admin.py
@@ -10,4 +10,16 @@ def write_into_csv(info_list):
 condition = True
 
 while condition:
+      student_info= input("Enter students information in the following order(Name, Age, Contact number,Email ID):")
+      print("Entered information:"+ student_info)
 
+      student_info_list=student_info.split('')
+      print("Entered split up list:"+str(student_info_list))
+
+      write_into_csv(student_info_list)
+
+      condition_check=input(Enter(Yes/No), if you want to continue for next student?)
+      if condition_check == "Yes":
+            condition = True
+      elif condition_check == "No":
+            condition = False
