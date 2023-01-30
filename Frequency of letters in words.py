0203ad64c25e647c4df22313b23938837d9ee434
Deepakchandhru <122144025+Deepakchandhru@users.noreply.github.com>
2023-01-30 17:47:58 +0530

    Update Frequency of letters in words.py

--- Frequency of letters in words.py
+++ Frequency of letters in words.py
@@ -1,9 +1,9 @@
 def most_frequent(a):
       f={}
       for i in range(len(a)):
-      	f[a[i]]=f.get(a[i],0)+1
+      	  f[a[i]]=f.get(a[i],0)+1
       for j in range(len(f)):
-      	l=max(f,key=f.get)
-      	print(l,":",f[l])
-      	f.pop(l,f[l])   	
+      	  l=max(f,key=f.get)
+          print(l,":",f[l])
+      	  f.pop(l,f[l])   	
 most_frequent(input("Enter word:"))  
